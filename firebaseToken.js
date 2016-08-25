dc6bb956b94cfd911fb9e104cdbb38ba0dffb4c5
Dan Sajjad <dan.sajjad@gmail.com>
2016-08-25 12:34:39 -0400

    Added function to generate token for firebase

---
+++ firebaseToken.js
@@ -0,0 +1,22 @@
+const Firebase = require('firebase');
+const FirebaseTokenGenerator = require('firebase-token-generator');
+const  config = require('./config.js')();
+
+let firebaseUrl;
+let firebaseSecret;
+
+// if(process.env.NODE_ENV !== 'production') {
+  firebaseUrl = config.firebase.FIREBASE_URL;
+  firebaseSecret = config.firebase.FIREBASE_SECRET;
+// } else {
+//   firebaseUrl = process.env.FIREBASE_URL;
+//   firebaseSecret = process.env.FIREBASE_SECRET;  
+// }
+
+
+
+var firebase = new Firebase(firebaseUrl);
+var tokenGenerator = new FirebaseTokenGenerator(firebaseSecret);
+var token = tokenGenerator.createToken({ uid: 'admin' });
+
+module.exports = token;
